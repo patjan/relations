8a7ce916f36976f935ffccfee4ba294ccd4e2c70
Pat Jan <pat-jan@hotmail.com>
2012-11-22 09:06:56 -0800

    new updates

--- html/js/controls.js
+++ html/js/controls.js
@@ -1,156 +1,204 @@
 /**
- *	JKY.controls_start
- *
- * 	start controls process
- *  initiated from main menu
+ * define JKY namespace for all application
  */
-JKY.controls_start = function() {
-	display_trace('JKY.controls_start');
-	//	if server session is gone, then redirect to Login Page
-	if (JKY.is_session_gone()){
-		JKY.router.transitionTo('login');
-		return;
-	}else{
-		JKY.controls_setup();
-	}
-}
+window.JKY = Em.Application.create({rootElement:'body'});
 
-/**
- *	JKY.controls_setup
- *
- * 	setup controls form
- *  initiated from JKY.control_start
- */
-JKY.controls_setup = function() {
-	display_trace('JKY.controls_setup');
-	if (Em.TEMPLATES["controls"]){
-JC.table            = 'Controls'        ;   //  DB table name
-JC.order_by         = 'control_name'    ;   //  first field name to be ordered by
-JC.order_seq        = 'ASC'             ;   //  first field name to be sequenced by ( ASC, DESC )
-JC.select_name      = 'Root'            ;   //  first field name to be selected
-JC.focus_name       = 'sequence'        ;   //  first field name to be focused
-JC.display_limit    = 'All'             ;   //  limit rows to be displayed
-		JKY.controls_get_data();
-	}else{
-		setTimeout(function(){JKY.controls_setup()}, 100);
-	}
-}	
+JKY.ApplicationController	= Em.Controller.extend();
+JKY. HeaderController		= Em.Controller.extend();
+JKY. FooterController		= Em.Controller.extend();
+JKY.ButtonsController		= Em.Controller.extend();
+JKY.   BodyController		= Em.Controller.extend();
+JKY.  TableController		= Em.Controller.extend();
 
-/**
- *	JKY.controls_get_data
- *
- * 	get data from server
- */
-JKY.controls_get_data = function(){
-	display_trace('JKY.controls_get_data');	
-filter = '';
-order_by = JC.order_by;
-specific = '';
-JC.select_name = 'User Resources';
-	var  data = {command:'get_index', table:JC.table, filter:filter, select:JC.select_name, display:JC.display_limit, order_by:order_by, specific:specific};
-	$('#jky_loading').show();
-    $.ajax({data:data, success:JKY.display_index});
-}
-	
-/**
- *	JKY.display_index
- *
- * 	display index
- */
-JKY.display_index = function(data, text_status, jq_XHR) {
-	display_trace('JKY.display_index');	
-	JC.skip_add_new = false;
-	$('#jky_loading').hide();
+JKY.ApplicationView	= Em.View.extend({templateName:'template-wrapper'	});
+JKY. HeaderView 	= Em.View.extend({templateName:'template-header'	});
+JKY. FooterView 	= Em.View.extend({templateName:'template-footer'	});
+JKY.ButtonsView 	= Em.View.extend({templateName:'template-buttons'	});
+JKY.   BodyView 	= Em.View.extend({templateName:'template-body'		});
+JKY.  TableView 	= Em.View.extend({templateName:'template-table'		});
 
-	if (data['status'] != 'ok') {
-		display_message(data['message']);
-		return;
-	}
+JKY.Router = Em.Router.extend(
+	{ enableLogging: true
+	, root: Em.Route.extend(
+		{ loadNewSet: Em.Route.transitionTo('newSet')
 
-	$.each(data, function(name, value) {
-		if (name == 'rows') {
-			JC.rows = value;
-//			JC.sort_by();
-			JC.display_rows();
+		, index: Em.Route.extend(
+			{ route: '/'
+			, connectOutlets: function(router, context) {
+				JKY.displayTrace('JKY.Router.index');
+				JKY.loadNewSet('Root');
+				}
+			})
+
+		, newSet: Em.Route.extend(
+			{ route: '/newSet/:name'
+			, deserialize	: function(router, context) {
+				return (context.name)
+				}
+			, serialize		: function(router, context) {
+				return {name: context}
+				}
+			, connectOutlets: function(router, controlSet) {
+				JKY.displayTrace('JKY.Router.newSet, newSet: ' + controlSet);
+				JKY.loadNewSet2(controlSet);
+				}
+			})
+		})
+	});
+
+$(function() {
+	$.ajaxSetup({
+		dataType: 'json',
+		error	: function(jqXHR, textStatus, errorThrown) {
+			JKY.hideLoading();
+			JKY.displayMessage('Error from backend server, please re-try later.');
 		}
-	})
+	});
+
+	JKY.displayTrace('$(function() {})');
+	JKY.controlsStart();
+});
+
+/**
+ * JKY.controlsStart
+ * wait until the template is compiled by handlebars
+ *
+ * initiate Ember Router and set bindings
+ */
+JKY.controlsStart = function() {
+	JKY.displayTrace('JKY.controlsStart');
+	JKY.router.get('applicationController').connectOutlet('outlet-header'	, 'header'	);
+	JKY.router.get('applicationController').connectOutlet('outlet-footer'	, 'footer'	);
+	JKY.router.get('applicationController').connectOutlet('outlet-buttons'	, 'buttons'	);
+	JKY.router.get('applicationController').connectOutlet('outlet-body'		, 'body'	);
+	JKY.router.get(       'bodyController').connectOutlet('outlet-table'	, 'table'	);
+	JKY.setHeader	();
+	JKY.setFooter	();
+	JKY.setButtons	();
+	JKY.setBody		();
+//	JKY.loadNewSet('Root');
 };
 
-    JC.table            = 'Controls'        ;   //  DB table name
-    JC.order_by         = 'control_name'    ;   //  first field name to be ordered by
-    JC.order_seq        = 'ASC'             ;   //  first field name to be sequenced by ( ASC, DESC )
-    JC.select_name      = 'Root'            ;   //  first field name to be selected
-    JC.focus_name       = 'sequence'        ;   //  first field name to be focused
-    JC.display_limit    = 'All'             ;   //  limit rows to be displayed
+/**
+ * load new set
+ */
+JKY.loadNewSet = function(controlSet) {
+	JKY.displayTrace('JKY.loadNewSet, controlSet: ' + controlSet);
+/*
+	$.ajax({
+		url		: JKY.AJAX_URL + 'GetUserRoles',
+		success	: function(response) {
+			JKY.displayTrace('JKY.loadNewSet, ajax, success');
+			if (response.Status != 'ok') {
+				JKY.displayMessage(response.Error.Message);
+			} else {
+				JKY.saveUserRoles(response.Data);
+			}
+		}
+	});
+*/
+	JKY.TableView = Em.View.extend(
+		{ table_body	:
+			[{ 'sequence': 0, 'control-name': 'Company Type', 'control-value': '', 'status': 'active'}
+			,{ 'sequence': 0, 'control-name': 'Display Rows', 'control-value': '', 'status': 'active'}
+			,{ 'sequence': 0, 'control-name': 'Group Types'	, 'control-value': '', 'status': 'active'}
+			,{ 'sequence': 0, 'control-name': 'Priorities'	, 'control-value': '', 'status': 'active'}
+			,{ 'sequence': 0, 'control-name': 'Root'		, 'control-value': '', 'status': 'active'}
+			,{ 'sequence': 0, 'control-name': 'Status Codes', 'control-value': '', 'status': 'active'}
+			]
+		});
+}
 
-JC.select_setup = function() {
-    $('#jky_display'    ).load(JC.ajax_url, {command:'get_options', control_set:'Display Rows', select:JC.display_limit, initial:'All'});
-    $('#status'         ).load(JC.ajax_url, {command:'get_options', control_set:'Status Codes'});
-//  $('#control_name'   ).change(JC.change);
-    JC.refresh_select();
-    JC.request_index ();
-};
+/**
+ * load new set
+ */
+JKY.loadNewSet2 = function(controlSet) {
+	JKY.displayTrace('JKY.loadNewSet2, controlSet: ' + controlSet);
+/*
+	$.ajax({
+		url		: JKY.AJAX_URL + 'GetUserRoles',
+		success	: function(response) {
+			JKY.displayTrace('JKY.loadNewSet, ajax, success');
+			if (response.Status != 'ok') {
+				JKY.displayMessage(response.Error.Message);
+			} else {
+				JKY.saveUserRoles(response.Data);
+			}
+		}
+	});
+*/
+	JKY.TableView = Em.View.extend(
+		{ table_body	:
+			[{ 'sequence': 10, 'control-name': 'Company Type', 'control-value': '', 'status': 'active'}
+			,{ 'sequence': 10, 'control-name': 'Display Rows', 'control-value': '', 'status': 'active'}
+			,{ 'sequence': 10, 'control-name': 'Group Types'	, 'control-value': '', 'status': 'active'}
+			,{ 'sequence': 10, 'control-name': 'Priorities'	, 'control-value': '', 'status': 'active'}
+			,{ 'sequence': 10, 'control-name': 'Root'		, 'control-value': '', 'status': 'active'}
+			,{ 'sequence': 10, 'control-name': 'Status Codes', 'control-value': '', 'status': 'active'}
+			]
+		});
+}
 
-JC.refresh_select = function() {
-    $('#jky_select').load(JC.ajax_url, {command:'get_options', control_set:'Root', select:JC.select_name});
-};
+JKY.setHeader = function() {
+	JKY.HeaderView = Em.View.extend(
+		{ company_name	: 'JKY Software Corp'
+		, company_logo	: 'relations'
+		, user_name		: 'Pat Jan'
+		, event_name	: '2012 Annual Event'
 
-JC.display_row = function(row) {
-    return ''
-        + '<td class=C>' + row['sequence'       ] + '</td>'
-        + '<td class=L>' + row['control_name'   ] + '</td>'
-        + '<td class=L>' + row['control_value'  ] + '</td>'
-        + '<td class=C>' + row['status'         ] + '</td>'
-        ;
-};
+		, setUserName	: function(user_name)	{this.user_name = user_name;}
+		, getUserName	: function()			{return this.user_name;		}
+		});
+}
 
-JC.set_new = function() {
-	$('#status'         ).val('active');
-	$('#control_set'    ).val($('#jky_select').val());
-	$('#sequence'       ).val(0);
-	$('#control_name'   ).val('');
-	$('#control_value'  ).val('');
-};
+JKY.setFooter = function() {
+	JKY.FooterView = Em.View.extend(
+		{ copyright		: '© 2013 JKY Software Corp'
+		, contact_us	: 'Contact Us'
+		});
+}
 
-JC.return_set = function() {
-	var error = '';
-	error += jky_verify('sequence'     );
-	error += jky_verify('control_name' );
+JKY.setButtons = function() {
+	JKY.ButtonsView = Em.View.extend(
+		{ menus		:
+			[{ label: 'Home'			, icon: 'home'			}
+			,{ label: 'Help'			, icon: 'question-sign'	}
+			,{ label: 'My Info'			, icon: 'user'			}
+			]
+		, admins	:
+			[{ label: 'Dictionary'		, icon: 'tasks'			}
+			,{ label: 'Summary'			, icon: 'tasks'			}
+			,{ label: 'Groups'			, icon: 'tasks'			}
+			,{ label: 'Templates'		, icon: 'tasks'			}
+			,{ label: 'Organizations'	, icon: 'tasks'			}
+			,{ label: 'Permissions'		, icon: 'tasks'			}
+			,{ label: 'Settings'		, icon: 'tasks'			}
+			,{ label: 'Controls'		, icon: 'tasks'			}
+			]
+		});
+}
 
-	if (error != '') {
-		JC.display_message(error, JC.focus_name);
-		return '';
-	} else {
-		return '            status=' + jky_get_text('status'       )
-			+ ',       control_set=' + jky_get_text('control_set'  )
-			+ ',          sequence=' + jky_get_text('sequence'     )
-			+ ',      control_name=' + jky_get_text('control_name' )
-			+ ',     control_value=' + jky_get_text('control_value')
-			;
-	}
-};
+JKY.setBody = function() {
+	JKY.BodyView = Em.View.extend(
+		{ name		: 'Controls'
+		, icon		: 'tasks'
+		, buttons	:
+			[{ label: 'Add New'			, icon: 'tasks'			}
+			,{ label: 'Export'			, icon: 'tasks'			}
+			,{ label: 'Publish'			, icon: 'tasks'			}
+			,{ label: 'Combine'			, icon: 'tasks'			}
+			,{ label: 'Upload'			, icon: 'tasks'			}
+			,{ label: 'Print'			, icon: 'tasks'			}
+			]
+		, filter	: 'filter...'
+		, loaded	: 123
+		, counter	: 1234
+		, table_header	:
+			[{ id: 'sequence'		, label: 'Seq'				}
+			,{ id: 'control-name'	, label: 'Control Name'		}
+			,{ id: 'control-value'	, label: 'Control Value'	}
+			,{ id: 'status'			, label: 'Status'			}
+			]
+		});
+}
 
-//   for every new control created, add new extra field(s)
-JC.prep_insert = function(row_id) {
-	return ', company_id= ' + JC.control_company;
-};
-
-JC.post_insert = function(row_id) {
-	JC.refresh_select();
-};
-
-JC.post_update = function(row_id) {
-	JC.refresh_select();
-};
-
-// -----------------------------------------------------------------------------
-JC.publish = function() {
-    var control_set = '';
-    if (JC.table == 'Controls')
-        control_set = JC.select_name;
-
-    var data = {command:'publish', table:JC.table, control_set:control_set};
-    $.ajax({data:data}).success(function(data) {
-        JC.display_message(data['message']);
-    });
-};
